9261b9c928ee2aae43ac573c842b5c80073dd1ef
zhong <zhongshun6666@163.com>
2019-09-07 15:30:40 +0800

    添加分页

--- server/settings.py
+++ server/settings.py
@@ -12,7 +12,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'guardian'
+    'guardian',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ),
     'DEFAULT_THROTTLE_CLASSES': [
         'public.throttles.AnonBurstRateThrottle',
         'public.throttles.UserBurstRateThrottle'
@@ -88,7 +86,12 @@ REST_FRAMEWORK = {
         'user-burst': '60/min',
         'anon': '500/day',
         'user': '2000/day'
-    }
+    },
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 LANGUAGE_CODE = 'zh-Hans'
--- users/views.py
+++ users/views.py
@@ -1,31 +1,29 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .permissions import IsSuperuser, IsOwner
 from .models import User
 from .serializers import UserSerializer, PasswordSerializer
+from .paginations import Pagination
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    @action(detail=True, methods=['post'])
-    def set_password(self, request, pk=None):
-        serializer = PasswordSerializer(data=request.data, context={'request': request})
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    pagination_class = Pagination
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['username']
+    ordering_fields = ['username']
+    ordering = ['username']
 
     def get_permissions(self):
-        request = self.request
         permission_classes = []
         if self.action == 'list':
             permission_classes = [IsAuthenticated]
         elif self.action == 'create':
-            is_create_staff = 'is_staff' in request.data and request.data['is_staff']
-            is_create_superuser = 'is_superuser' in request.data and request.data['is_superuser']
-            permission_classes = [IsSuperuser] if is_create_staff or is_create_superuser else []
+            pass
         elif self.action == 'retrieve':
             pass
         elif self.action in ['update', 'partial_update', 'destroy']:
@@ -33,3 +31,10 @@ class UserViewSet(viewsets.ModelViewSet):
         elif self.action == 'set_password':
             permission_classes = [IsAuthenticated | IsSuperuser]
         return [permission() for permission in permission_classes]
+
+    @action(detail=True, methods=['post'])
+    def set_password(self, request, pk=None):
+        serializer = PasswordSerializer(data=request.data, context={'request': request})
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
