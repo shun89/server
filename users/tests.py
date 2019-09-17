fb8f69a64b25930da1254da5df5582aaa73fb151
zhong <zhongshun6666@163.com>
2019-09-18 07:45:57 +0800

    添加测试模块(未完成)

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,9 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
 from .models import User
+from .views import UserViewSet
 
 
 class TokenTests(APITestCase):
@@ -17,24 +19,40 @@ class TokenTests(APITestCase):
     def test_token_auth(self):
         url = reverse('token-auth')
         data = {'username': self.username, 'password': self.password}
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 class UserTests(APITestCase):
     def setUp(self):
-        self.username = 'test'
-        self.password = '123456'
-        self.email = 'test@qq.com'
+        username = 'test'
+        password = '123456'
+        email = 'test@qq.com'
+        self.user = User.objects.create_user(
+            username,
+            email,
+            password
+        )
+        url = reverse('token-auth')
+        data = {'username': username, 'password': password}
+        response = self.client.post(url, data)
+        token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
 
     def test_create(self):
         data = {
-            'username': self.username,
-            'password': self.password,
-            'email': self.email
+            'username': 'creator',
+            'password': '123456',
+            'email': 'creator@qq.com'
         }
         response = self.client.post('/api/users/', data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list(self):
+        # view = UserViewSet.as_view({'get': 'list'})
+        url = reverse('user-set-password')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve(self):
         pass
