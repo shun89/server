97d475609a8a72d43b0d283b22a0a6efe6dea872
zhong <zhongshun6666@163.com>
2020-01-07 18:31:06 +0800

    添加更新头像，更新密码的功能

--- users/serializers.py
+++ users/serializers.py
@@ -2,19 +2,6 @@ from rest_framework import serializers
 from .models import User
 
 
-class PasswordSerializer(serializers.Serializer):
-    password = serializers.CharField(min_length=6)
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-    def update(self, instance, validated_data):
-        pass
-
-
 class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .permissions import IsSuperuser, IsOwner
 from .models import User
-from .serializers import UserSerializer, PasswordSerializer
+from .serializers import UserSerializer
 from .paginations import Pagination
 
 
@@ -20,11 +20,6 @@ class UserViewSet(viewsets.ModelViewSet):
     ordering_fields = ['create_at', 'is_active', 'is_superuser', 'is_staff']
     ordering = ['-create_at']
 
-    # create user with avatar by post
-    @parser_classes([MultiPartParser, FormParser])
-    def create(self, request, *args, **kwargs):
-        return super(UserViewSet, self).create(request, *args, **kwargs)
-
     def get_permissions(self):
         permission_classes = []
         if self.action == 'list':
@@ -35,15 +30,28 @@ class UserViewSet(viewsets.ModelViewSet):
             pass
         elif self.action in ['update', 'partial_update', 'destroy']:
             permission_classes = [IsOwner | IsSuperuser]
-        elif self.action == 'set_password':
+        elif self.action in ['set_password', 'update_avatar']:
             permission_classes = [IsOwner | IsSuperuser]
         return [permission() for permission in permission_classes]
 
-    @action(detail=True, methods=['post'])
+    # create user with avatar by post
+    @parser_classes([MultiPartParser, FormParser])
+    def create(self, request, *args, **kwargs):
+        return super(UserViewSet, self).create(request, *args, **kwargs)
+
+    @parser_classes([MultiPartParser, FormParser])
+    @action(detail=True, methods=['put'])
+    def update_avatar(self, request, *args, **kwargs):
+        return super(UserViewSet, self).partial_update(request, *args, **kwargs)
+
+    @action(detail=False, methods=['post'])
     def set_password(self, request, pk=None):
-        serializer = PasswordSerializer(data=request.data, context={'request': request})
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        user = request.user
+        if user.check_password(request.data['oldPassword']):
+            user.set_password(request.data['newPassword'])
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(data={'detail': '老密码输入不正确!'}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=['get'])
     def get_auth_user(self, request, pk=None):
